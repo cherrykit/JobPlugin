708e40f7b7fad4c5d4b032f42c6aa1a179a48871
Cherry <cherrykit@outlook.com>
2017-01-27 10:00:06 +0100

    Improved code structure

--- src/main/java/me/cherrykit/Main.java
+++ src/main/java/me/cherrykit/Main.java
@@ -25,86 +25,106 @@ public class Main extends JavaPlugin implements Listener {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
+		//Registers /balance command and sends message to player
 		if (cmd.getName().equalsIgnoreCase("balance") && sender instanceof Player) {
 			Player p = (Player) sender;
 			p.sendMessage(ChatColor.GREEN + "Your balance is " + SQLData.getMoney(p.getName()));
 		}
 		
+		//Registers /jobs commands
 		if (cmd.getName().equalsIgnoreCase("jobs") && sender instanceof Player) {
-            Player p = (Player) sender;
-            
-            String type;
-            try {
-            	type = args[0];
-            } catch (Exception e) {
-            	type = null;
-            }
-            
-            //Differs between breaker and builder
-            String job;
-        	try {
-            	job = args[1];
-            } catch (Exception e) {
-            	job = null;
-            }
-            
-            //Differs between join, leave and info
-            switch (type) {
-            case "join" :
-            	if (SQLData.getJob(p.getName())[0] == "0") {
-            		if (job.equalsIgnoreCase("builder")) {
-            			SQLData.setJob(p.getName(),"builder",0);
-                		p.sendMessage(ChatColor.GOLD + "You have joined the job builder.");
-            		} 
-            		else if (job.equalsIgnoreCase("breaker")) {
-                		SQLData.setJob(p.getName(), "breaker", 0);
-                		p.sendMessage(ChatColor.GOLD + "You have joined the job breaker.");
-                	} 
-            		else {
-                		p.sendMessage(ChatColor.RED + "Invalid job name. Valid jobs: breaker, builder");
-                	}
-            	}
-            	else {
+			Player p = (Player) sender;
+			
+			//Registers type of command
+			String type;
+			try {
+				type = args[0];
+			} catch (Exception e) {
+				type = null;
+			}
+			
+			//Registers name of job
+			String job;
+			try {
+				job = args[1];
+			} catch (Exception e) {
+				job = null;
+			}
+			
+			//Differs between join, leave and info
+			switch (type) {
+			
+				//If command was /jobs join
+				case "join" :
+					//If player doesn't have job yet, sets job
+					if (SQLData.getJob(p.getName())[0] == "0") {
+						//Joins builder
+						if (job.equalsIgnoreCase("builder")) {
+							SQLData.setJob(p.getName(),"builder",0);
+							p.sendMessage(ChatColor.GOLD + "You have joined the job builder.");
+						} 
+						//Joins breaker
+						else if (job.equalsIgnoreCase("breaker")) {
+							SQLData.setJob(p.getName(), "breaker", 0);
+							p.sendMessage(ChatColor.GOLD + "You have joined the job breaker.");
+						} 
+						
+						else {
+							p.sendMessage(ChatColor.RED + "Invalid job name. Valid jobs: breaker, builder");
+						}
+					} else {
         			p.sendMessage(ChatColor.RED + "You already have a job.");
-        		}
-            	
-            	break;
-            	
-            case "leave" :
-            	SQLData.removeJob(p.getName());
-            	p.sendMessage(ChatColor.GOLD + "You have left your job.");
-            	break;
-            	
-            case "info" :
-            	if (job.equalsIgnoreCase("builder")) {
-            		p.sendMessage(ChatColor.AQUA + "Builder gives you money for every block you place. You get 1% more "
-            				+ "money every 100 blocks.");
-            	}
-            	else if (job.equalsIgnoreCase("breaker")) {
-            		p.sendMessage(ChatColor.AQUA + "Builder gives you money for every block you break. You get 1% more "
-            				+ "money every 100 blocks.");
-            	} else {
-            		p.sendMessage(ChatColor.RED + "Invalid job name. Valid jobs: breaker, builder");
-            	}
-            	break;
-            	
-            default:
-            	p.sendMessage(ChatColor.RED + "Syntax: /jobs <join/leave/info> <builder/breaker>");
-            }
-            
-            return true;
-        }
-		
+					}
+					
+					break;
+				
+				//If command was /jobs leave
+				case "leave" :
+					//Leaves job
+					SQLData.removeJob(p.getName());
+					p.sendMessage(ChatColor.GOLD + "You have left your job.");
+					
+					break;
+				
+				//If command was /jobs info
+				case "info" :
+					//Gives info to builder
+					if (job.equalsIgnoreCase("builder")) {
+						p.sendMessage(ChatColor.AQUA + "Builder gives you money for every block you place. "
+							     + "You get 1% more money every 100 blocks.");
+					}
+					//Gives info to breaker
+					else if (job.equalsIgnoreCase("breaker")) {
+						p.sendMessage(ChatColor.AQUA + "Builder gives you money for every block you break. "
+							      + "You get 1% more money every 100 blocks.");
+					
+					} else {
+						p.sendMessage(ChatColor.RED + "Invalid job name. Valid jobs: breaker, builder");
+					}
+					
+					break;
+			
+				//If invalid command:
+				default:
+					p.sendMessage(ChatColor.RED + "Syntax: /jobs <join/leave/info> <builder/breaker>");
+			}
+			return true;
+		}
 		return false;
 	}
 	
+	//Registers blocks being placed
 	@EventHandler
 	public void onPlaceBlock(BlockPlaceEvent e) {
+		//Get players job
 		String pname = e.getPlayer().getName();
 		String[] currentJob = SQLData.getJob(pname);
 		
+		//If player is builder
 		if (currentJob[0].equals("builder")) {
+			//Updates blockamount
 			SQLData.setJob(pname, currentJob[0], Integer.parseInt(currentJob[1]) + 1);
+			//Determines and sets amount of money earned
 			int percentage = (int) ((Integer.parseInt(currentJob[1]) + 1) * 0.01 + 100);
 			double money = Double.parseDouble(SQLData.getMoney(pname)) + 1 * (percentage/100.0);
 			SQLData.setMoney(pname, money);
@@ -112,13 +132,18 @@ public class Main extends JavaPlugin implements Listener {
 		
 	}
 	
+	//Registers blocks being broken
 	@EventHandler
 	public void onBreakBlock(BlockBreakEvent e) {
+		//Gets players job
 		String pname = e.getPlayer().getName();
 		String[] currentJob = SQLData.getJob(pname);
 		
+		//If player is breaker
 		if (currentJob[0].equals("breaker")) {
+			//Updates blockamount
 			SQLData.setJob(pname, currentJob[0], Integer.parseInt(currentJob[1]) + 1);
+			//Determines and sets amount of money earned
 			int percentage = (int) ((Integer.parseInt(currentJob[1]) + 1) * 0.01 + 100);
 			double money = Double.parseDouble(SQLData.getMoney(pname)) + 1 * (percentage/100.0);
 			SQLData.setMoney(pname, money);
